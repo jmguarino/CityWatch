08acfa319d1fb2d88d48af05e69207cd000b17aa
Justin M. Guarino <jmguarino89@gmail.com>
2013-09-11 00:35:45 -0700

    This is with the very early stages of the MainActivity, Report activity (which doesn't include anything in the listview, so we'll have to explain the large blank space during the presentation), and the Settings activity.
    Currently the CityWatch title along the top does not show up when the
    app is launched (the theme set in the manifest is NoTitle), but it does
    show up in the graphical preview in Eclipse however.

--- CityWatch/src/jgnetworks/net/citywatch/MainActivity.java
+++ CityWatch/src/jgnetworks/net/citywatch/MainActivity.java
@@ -1,15 +1,41 @@
 package jgnetworks.net.citywatch;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    private Button report;
+    private Button settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        report = (Button) findViewById(R.id.report);
+        settings = (Button) findViewById(R.id.settings);
+
+        //Loads the report activity
+        report.setOnClickListener(new View.OnClickListener() {
+            Intent myIntent = new Intent(MainActivity.this, Report.class);
+            @Override
+            public void onClick(View arg0) {
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            Intent myIntent = new Intent(MainActivity.this, Settings.class);
+            @Override
+            public void onClick(View arg0) {
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 
     @Override
---
+++ CityWatch/src/jgnetworks/net/citywatch/Settings.java
@@ -0,0 +1,22 @@
+package jgnetworks.net.citywatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.report, menu);
+        return true;
+    }*/
+
+}
