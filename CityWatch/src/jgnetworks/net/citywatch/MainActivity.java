a2fc8d00f0f096f99e4b8b544af9964346181e1f
Justin M. Guarino <jmguarino89@gmail.com>
2013-09-10 22:57:11 -0700

    After poking around with the main activity interface for a bit...

--- CityWatch/src/jgnetworks/net/citywatch/MainActivity.java
+++ CityWatch/src/jgnetworks/net/citywatch/MainActivity.java
@@ -1,7 +1,7 @@
 package jgnetworks.net.citywatch;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
