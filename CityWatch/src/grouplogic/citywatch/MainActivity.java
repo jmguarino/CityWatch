569c9d86d56caf86718ff5a72863bd2c60ea4165
Kyle Janssen <kjanssen@seawolf.sonoma.edu>
2013-09-14 21:41:51 -0700

    Changed the way the settings and report activites are launched.

--- CityWatch/src/grouplogic/citywatch/MainActivity.java
+++ CityWatch/src/grouplogic/citywatch/MainActivity.java
@@ -6,38 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends Activity {
-    private Button report;
-    private Button settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        report = (Button) findViewById(R.id.report);
-        settings = (Button) findViewById(R.id.settings);
-
-        //Loads the report activity
-        report.setOnClickListener(new View.OnClickListener() {
-            Intent myIntent = new Intent(MainActivity.this, Report.class);
-            @Override
-            public void onClick(View arg0) {
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
-
-        //Loads settings activity
-        settings.setOnClickListener(new View.OnClickListener() {
-            Intent myIntent = new Intent(MainActivity.this, Settings.class);
-            @Override
-            public void onClick(View arg0) {
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
     }
 
     @Override
@@ -45,6 +20,16 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void openReport(View view) {
+    	Intent intent = new Intent(this, Report.class);
+    	startActivity(intent);
+    }
+    
+    public void openSettings(View view) {
+    	Intent intent = new Intent(this, Settings.class);
+    	startActivity(intent);
     }
 
 }
